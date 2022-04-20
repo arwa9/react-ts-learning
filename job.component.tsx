07a3a8151dd283a60cbe5c63dcb8f4ebfddee205
arwa9 <marcos.arwa9@gmail.com>
2022-04-20 18:20:57 +0200

    adding job details

--- job.component.tsx
+++ job.component.tsx
@@ -3,7 +3,23 @@ import { Job } from './model/job.interface';
 
 type PropsType = {
   job: Job;
-}
+};
+const newDate: Date = new Date();
+
 export const JobComponent = ({ job }: PropsType) => {
-  return <h1>{job.title}</h1>;
-}
+  return (
+    <div className="job">
+      <div className="job__img-container">
+        <img />
+      </div>
+      <div className="job__details">
+        <span className="company-name">{job.company.name}</span>
+        {job?.date?.getDate() <= newDate.getDate() - 2 && (
+          <span className="new-tag">NEW</span>
+        )}
+        <span className="featured-tag">FEATURED</span>
+      </div>
+      <div className="job__tags"></div>
+    </div>
+  );
+};
