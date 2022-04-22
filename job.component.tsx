9229ab13d57e478815346a204d0cd166565c2630
arwa9 <marcos.arwa9@gmail.com>
2022-04-22 14:44:43 +0200

    Adding data visualization

--- job.component.tsx
+++ job.component.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Job } from './model/job.interface';
+import { ContractTypes, Job } from './model/job.interface';
+import './job.component.scss';
 
 type PropsType = {
   job: Job;
 };
-const newDate: Date = new Date();
+
+const getDaysDiff = (date: Date | string): number => {
+  const d: Date = typeof date === 'string' ? new Date(date) : date;
+  return Math.ceil(
+    Math.abs((new Date().getTime() - d.getTime()) / (24 * 60 * 60 * 1000))
+  );
+};
 
 export const JobComponent = ({ job }: PropsType) => {
   return (
@@ -12,12 +19,18 @@ export const JobComponent = ({ job }: PropsType) => {
       <div className="job__img-container">
         <img />
       </div>
-      <div className="job__details">
-        <span className="company-name">{job.company.name}</span>
-        {job?.date?.getDate() <= newDate.getDate() - 2 && (
-          <span className="new-tag">NEW</span>
-        )}
+      <div className="job__info">
+        <h5 className="company-name">{job.company.name}</h5>
+        {getDaysDiff(job.date) <= 2 && <span className="new-tag">NEW</span>}
         <span className="featured-tag">FEATURED</span>
+        <br />
+        <h3 className="title">{job.title}</h3>
+        <span className="details">
+          {getDaysDiff(job.date)} days ago &nbsp;·&nbsp;
+          {job.contractType}
+          &nbsp;·&nbsp;
+          {job.location}
+        </span>
       </div>
       <div className="job__tags"></div>
     </div>
